606bce89e49ef25cb25d83c87641b9f84f915efb
ferrysyukmana <ferrysyukmana02@gmail.com>
2022-02-09 20:09:45 +0700

    first katalon

---
+++ Object Repository/Page_Google/img_google.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_google</name>
+   <tag></tag>
+   <elementGuidId>bd8f28df-dfa2-4a65-aed3-a03a3902151b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//img[@alt=&quot;Google&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//img[@alt=&quot;Google&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
